1625d360858e58c4bff071409bbf52e0e9448e6c
BotBw <wang1570@e.ntu.edu.sg>
2022-12-30 16:10:55 +0800

    aba

--- run.py
+++ run.py
@@ -5,14 +5,191 @@ from albumentations.pytorch.transforms import ToTensorV2
 
 from easydict import EasyDict as edict
 
-from models import *  # set ONNX_EXPORT in models.py
-from utils.datasets import *
-from utils.utils import *
+import torch
+import torchvision
 
 import os
 import logging
 import json
 import numpy as np
+import time
+import cv2
+
+def time_synchronized():
+    torch.cuda.synchronize() if torch.cuda.is_available() else None
+    return time.time()
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+def box_iou(box1, box2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
+
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(box1.t())
+    area2 = box_area(box2.t())
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) - torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    return inter / (area1[:, None] + area2 - inter)  # iou = inter / (area1 + area2 - inter)
+
+
+def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.6, multi_label=True, classes=None, agnostic=False):
+    """
+    Performs  Non-Maximum Suppression on inference results
+    Returns detections with shape:
+        nx6 (x1, y1, x2, y2, conf, cls)
+    """
+
+    # Settings
+    merge = True  # merge for best mAP
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    time_limit = 10.0  # seconds to quit after
+
+    t = time.time()
+    nc = prediction[0].shape[1] - 5  # number of classes
+    multi_label &= nc > 1  # multiple labels per box
+    output = [None] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        x = x[x[:, 4] > conf_thres]  # confidence
+        x = x[((x[:, 2:4] > min_wh) & (x[:, 2:4] < max_wh)).all(1)]  # width-height
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[..., 5:] *= x[..., 4:5]  # conf = obj_conf * cls_conf
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero().t()
+            x = torch.cat((box[i], x[i, j + 5].unsqueeze(1), j.float().unsqueeze(1)), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1)
+            x = torch.cat((box, conf.unsqueeze(1), j.float().unsqueeze(1)), 1)[conf > conf_thres]
+
+        # Filter by class
+        if classes:
+            x = x[(j.view(-1, 1) == torch.tensor(classes, device=j.device)).any(1)]
+
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
+
+        # If none remain process next image
+        n = x.shape[0]  # number of boxes
+        if not n:
+            continue
+
+        # Sort by confidence
+        # x = x[x[:, 4].argsort(descending=True)]
+
+        # Batched NMS
+        c = x[:, 5] * 0 if agnostic else x[:, 5]  # classes
+        boxes, scores = x[:, :4].clone() + c.view(-1, 1) * max_wh, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.boxes.nms(boxes, scores, iou_thres)
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            try:  # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+                iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+                weights = iou * scores[None]  # box weights
+                x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+                # i = i[iou.sum(1) > 1]  # require redundancy
+            except:  # possible CUDA error https://github.com/ultralytics/yolov3/issues/1139
+                print(x, i, x.shape, i.shape)
+                pass
+
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            break  # time limit exceeded
+
+    return output
+
+def clip_coords(boxes, img_shape):
+    # Clip bounding xyxy bounding boxes to image shape (height, width)
+    boxes[:, 0].clamp_(0, img_shape[1])  # x1
+    boxes[:, 1].clamp_(0, img_shape[0])  # y1
+    boxes[:, 2].clamp_(0, img_shape[1])  # x2
+    boxes[:, 3].clamp_(0, img_shape[0])  # y2
+
+
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = max(img1_shape) / max(img0_shape)  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2]] -= pad[0]  # x padding
+    coords[:, [1, 3]] -= pad[1]  # y padding
+    coords[:, :4] /= gain
+    clip_coords(coords, img0_shape)
+    return coords
+
+
+
+def detect(im0):
+    ret = []
+    # Run inference
+    t0 = time.time()
+    trans = A.Compose([
+                A.Resize(height=512, width=512, p=1.0),
+                # ToTensorV2(p=1.0),
+            ], p=1.0)
+    sample = {'image': im0}
+    img = trans(**(sample))['image']
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    img = img[None]
+
+    # Inference
+    t1 = time_synchronized()
+    pred = sess.run(['modelOutput'], {'modelInput': img})
+    t2 = time_synchronized()
+
+
+    # Apply NMS
+    pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
+                                multi_label=False, classes=opt.classes, agnostic=opt.agnostic_nms)
+    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
+    for bbox in pred:
+      if bbox is not None:
+          # Rescale boxes from imgsz to im0 size
+          bbox[:, :4] = scale_coords(img.shape[2:], bbox[:, :4], im0.shape).round()
+
+          # Write results
+          for *xyxy, conf, cls in bbox:
+            ret.append(('%g ' * 5) % (cls, *xyxy))
+
+    # Print time (inference + NMS)
+    logging.info('Done. (%.3fs)' % (time.time() - t0))
+    return ret
+
+
 
 def init():
     """
@@ -63,44 +240,6 @@ def run(raw_data):
     ans = detect(im0)
     logging.info("Request processed")
     return ans
-
-
-
-def detect(im0):
-    ret = []
-    # Run inference
-    t0 = time.time()
-    trans = A.Compose([
-                A.Resize(height=512, width=512, p=1.0),
-                # ToTensorV2(p=1.0),
-            ], p=1.0)
-    sample = {'image': im0}
-    img = trans(**(sample))['image']
-    img /= 255.0  # 0 - 255 to 0.0 - 1.0
-    img = img[None]
-
-    # Inference
-    t1 = torch_utils.time_synchronized()
-    pred = sess.run(['modelOutput'], {'modelInput': img})
-    t2 = torch_utils.time_synchronized()
-
-
-    # Apply NMS
-    pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
-                                multi_label=False, classes=opt.classes, agnostic=opt.agnostic_nms)
-    gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  #  normalization gain whwh
-    for bbox in pred:
-      if bbox is not None:
-          # Rescale boxes from imgsz to im0 size
-          bbox[:, :4] = scale_coords(img.shape[2:], bbox[:, :4], im0.shape).round()
-
-          # Write results
-          for *xyxy, conf, cls in bbox:
-            ret.append(('%g ' * 5) % (cls, *xyxy))
-
-    # Print time (inference + NMS)
-    logging.info('Done. (%.3fs)' % (time.time() - t0))
-    return ret
 
 
 # if __name__ == '__main__':
